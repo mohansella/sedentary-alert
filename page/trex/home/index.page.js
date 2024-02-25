00ffd3283a93654b82e23778e1ac9a6ce5f1297f
mohankumar.os <mohankumar.os@zohocorp.com>
2024-02-25 23:03:10 +0530

    using auto gui

--- page/trex/home/index.page.js
+++ page/trex/home/index.page.js
@@ -1,14 +1,46 @@
-import { createWidget, widget } from '@zos/ui'
-import { log as Logger } from '@zos/utils'
-import { TEXT_STYLE } from './index.style'
+import { align } from '@zos/ui';
+import { log as Logger } from '@zos/utils';
+import AutoGUI from '@silver-zepp/autogui';
 
 const logger = Logger.getLogger("SedentaryAlert");
 Page({
+  
+  state: {
+    activitySoFar: 0, //steps count
+    activityNeeded: 100, //steps count
+    lastActive: 0,  //UTC timestamp
+    alertIn: 0, //UTC timestamp
+  },
+
+  buildState() { //custom function
+
+    const activityString = this.state.activityNeeded == 0 ? "-" : (this.state.activitySoFar + "/" + this.state.activityNeeded );
+
+    const gui = new AutoGUI();
+    const options = { align_h: align.CENTER_H };
+
+    gui.spacer(); gui.newRow();
+    const activeTargetTitle = gui.text("Activity Target", options); gui.newRow();
+    const activeTargetValue = gui.text(activityString, options); gui.newRow();
+    
+    gui.spacer(); gui.newRow();
+
+    const alertInTitle = gui.text("Alert In", options); gui.newRow();
+    const alertInValue = gui.text("-", options); gui.newRow();
+    
+    gui.spacer(); gui.newRow();
+    
+    const lastActiveTitle = gui.text("Last Active", options); gui.newRow();
+    const lastActiveValue = gui.text("-", options); gui.newRow();
+    gui.spacer(); gui.newRow();
+    
+    
+    gui.render();
+  },
+
   build() {
-    logger.debug('page build invoked')
-    createWidget(widget.TEXT, {
-      ...TEXT_STYLE,
-    });
+    logger.debug('page build invoked');
+    this.buildState();
   },
   onInit() {
     logger.debug("page onInit invoked");
